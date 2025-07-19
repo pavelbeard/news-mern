a1345ba850a2d1f83df1c049b8bed7b5e69e2054
pavelbeard <pavelborodin1995@gmail.com>
2025-07-19 18:49:00 +0200

    fix: update ALLOWED_ORIGINS to include Netlify and Vercel frontend URLs

--- apps/api/src/lib/settings.ts
+++ apps/api/src/lib/settings.ts
@@ -1,3 +1,5 @@
-export const ALLOWED_ORIGINS = ["http://localhost:5173"].concat(
-  process.env?.ALLOWED_ORIGINS?.split(/,|\s/)
-);
+export const ALLOWED_ORIGINS = [
+  "http://localhost:5173",
+  "https://news-mern-backend.netlify.app/",
+  "https://news-mern-frontend-brown.vercel.app/",
+].concat(process.env?.ALLOWED_ORIGINS?.split(/,|\s/));
