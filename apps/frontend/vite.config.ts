f71647efd59af4e6f9fd7a2ea68b6e2701e2a1cd
pavelbeard <pavelborodin1995@gmail.com>
2025-07-19 04:22:09 +0200

    fix: test 10, @vercel/react-router doesn't work, I try to use vercel config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from "@tailwindcss/vite";
-import { vercelPreset } from "@vercel/react-router/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import oxlintPlugin from "vite-plugin-oxlint";
@@ -7,11 +6,5 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    oxlintPlugin(),
-    tailwindcss(),
-    viteTsconfigPaths(),
-    vercelPreset(),
-  ],
+  plugins: [react(), oxlintPlugin(), tailwindcss(), viteTsconfigPaths()],
 });
