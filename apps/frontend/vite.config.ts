e5504ecf1faaacd210bfae4c5352a4f552293160
pavelbeard <pavelborodin1995@gmail.com>
2025-07-19 04:13:12 +0200

    fix: test 9, vercel react router preset should fix the fails

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import { vercelPreset } from "@vercel/react-router/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import oxlintPlugin from "vite-plugin-oxlint";
@@ -6,5 +7,11 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), oxlintPlugin(), tailwindcss(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    oxlintPlugin(),
+    tailwindcss(),
+    viteTsconfigPaths(),
+    vercelPreset(),
+  ],
 });
