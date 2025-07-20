a67fafdeb5683a452d043dfc11b45b8995e0a3f1
pavelbeard <pavelborodin1995@gmail.com>
2025-07-20 22:39:03 +0200

    fix: update allowed origins and correct path in tsconfig.json

--- apps/backend/src/lib/settings.ts
+++ apps/backend/src/lib/settings.ts
@@ -1,6 +1,6 @@
 export const ALLOWED_ORIGINS = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://news-mern-backend.netlify.app/",
+  "https://news-mern-backend.vercel.app/",
   "https://news-mern-frontend.vercel.app/",
 ].concat(process.env?.ALLOWED_ORIGINS?.split(/,|\s/));
