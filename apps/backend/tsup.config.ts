db4c3d23fdf06b046546f9be05b2dd66122d6a35
pavelbeard <pavelborodin1995@gmail.com>
2025-07-20 13:37:55 +0200

    fix: update package.json and tsup.config.ts for correct paths and entry points

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entry: ["api/**/*"],
+  entry: ["api/**/*", "src/**/*"],
   clean: true,
   target: "node22",
   format: ["cjs"],
